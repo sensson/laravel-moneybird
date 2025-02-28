341adde0ed3081b7b971bd809eb61af27c6cd87e
ju5t <m@ju5t.nl>
2025-02-28 13:56:22 +0100

    refactor: update facade to access both connectors

--- src/Facades/Moneybird.php
+++ src/Facades/Moneybird.php
@@ -3,11 +3,21 @@
 namespace Sensson\Moneybird\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Sensson\Moneybird\Connectors\AuthConnector;
+use Sensson\Moneybird\Connectors\MoneybirdConnector;
 
+/**
+ * @see MoneybirdConnector
+ */
 class Moneybird extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
-        return '';
+        return MoneybirdConnector::class;
+    }
+
+    public static function auth(): AuthConnector
+    {
+        return new AuthConnector;
     }
 }
