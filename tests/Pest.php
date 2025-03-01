476cb1ab181307a7ec291fc0b4fd6e4fce796743
ju5t <m@ju5t.nl>
2025-03-01 16:01:18 +0100

    chore: clean up is done by saloon itself

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,10 +1,8 @@
 <?php
 
 use Saloon\Config;
-use Saloon\Http\Faking\MockClient;
 use Sensson\Moneybird\Tests\TestCase;
 
 Config::preventStrayRequests();
 
 uses(TestCase::class)->in(__DIR__);
-uses()->beforeEach(fn () => MockClient::destroyGlobal())->in(__DIR__);
