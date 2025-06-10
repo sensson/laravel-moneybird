ab349baa6e798b5159b2c119817695a64acbb8a6
ju5t <m@ju5t.nl>
2025-06-10 18:53:53 +0200

    tests: add fakes

--- src/Facades/Moneybird.php
+++ src/Facades/Moneybird.php
@@ -3,6 +3,8 @@
 namespace Sensson\Moneybird\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Saloon\Http\Connector;
+use Saloon\Http\Faking\MockClient;
 use Sensson\Moneybird\Connectors\AuthConnector;
 use Sensson\Moneybird\Connectors\MoneybirdConnector;
 
@@ -16,8 +18,23 @@ class Moneybird extends Facade
         return MoneybirdConnector::class;
     }
 
-    public static function auth(): AuthConnector
+    public static function auth(): AuthConnector|Connector
     {
-        return new AuthConnector;
+        return resolve(AuthConnector::class);
+    }
+
+    public static function authFake(MockClient $mockClient)
+    {
+        return app()->instance(AuthConnector::class, (new AuthConnector)->withMockClient($mockClient));
+    }
+
+    public static function fake(MockClient $mockClient)
+    {
+        return static::swap((new MoneybirdConnector)->withMockClient($mockClient));
+    }
+
+    public static function make(): MoneybirdConnector
+    {
+        return static::getFacadeRoot();
     }
 }
