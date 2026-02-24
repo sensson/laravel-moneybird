3a957098f576afccab98f1f00eeab212a63e31dc
ju5t <m@ju5t.nl>
2026-02-24 11:45:34 +0100

    test: add access token revoked exception tests

--- tests/Connectors/MoneybirdConnectorTest.php
+++ tests/Connectors/MoneybirdConnectorTest.php
@@ -1,6 +1,11 @@
 <?php
 
+use Saloon\Exceptions\Request\Statuses\UnauthorizedException;
+use Saloon\Http\Faking\MockClient;
+use Saloon\Http\Faking\MockResponse;
 use Sensson\Moneybird\Connectors\MoneybirdConnector;
+use Sensson\Moneybird\Exceptions\AccessTokenRevokedException;
+use Sensson\Moneybird\Requests\Administrations\ListAdministrations;
 
 test('connector has correct base url', function () {
     $connector = new MoneybirdConnector;
@@ -29,3 +34,21 @@ test('connector accepts json by default', function () {
     $traits = class_uses_recursive(get_class($connector));
     expect($traits)->toContain(\Saloon\Traits\Plugins\AcceptsJson::class);
 });
+
+it('throws AccessTokenRevokedException when access token is revoked', function () {
+    $mockClient = new MockClient([
+        ListAdministrations::class => MockResponse::make('access token revoked', 401),
+    ]);
+
+    $connector = (new MoneybirdConnector)->withMockClient($mockClient);
+    $connector->send(new ListAdministrations);
+})->throws(AccessTokenRevokedException::class);
+
+it('does not throw AccessTokenRevokedException for other 401 responses', function () {
+    $mockClient = new MockClient([
+        ListAdministrations::class => MockResponse::make('unauthorized', 401),
+    ]);
+
+    $connector = (new MoneybirdConnector)->withMockClient($mockClient);
+    $connector->send(new ListAdministrations);
+})->throws(UnauthorizedException::class);
